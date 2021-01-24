72cf173eb343f448cc57edbfafca2b5dbd90c028
batuhansevinc <eyupbatuhansevinc@gmail.com>
2021-01-24 23:47:33 +0300

    ss

--- assets/js/creative.js
+++ assets/js/creative.js
@@ -60,4 +60,41 @@
     }
   });
 
+  $().ready(function(){
+    $('.slick-carousel').slick({
+      arrows: true,
+      centerPadding: "0px",
+      dots: true,
+      infinite: true,
+      slidesToShow: 3,
+      centerMode: true
+    });
+  });
+
 })(jQuery); // End of use strict
+
+/*
+    Carousel
+*/
+$('#carousel-example').on('slide.bs.carousel', function (e) {
+  /*
+      CC 2.0 License Iatek LLC 2018 - Attribution required
+  */
+  var $e = $(e.relatedTarget);
+  var idx = $e.index();
+  var itemsPerSlide = 5;
+  var totalItems = $('.carousel-item').length;
+
+  if (idx >= totalItems-(itemsPerSlide-1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i=0; i<it; i++) {
+          // append slides to end
+          if (e.direction=="left") {
+              $('.carousel-item').eq(i).appendTo('.carousel-inner');
+          }
+          else {
+              $('.carousel-item').eq(0).appendTo('.carousel-inner');
+          }
+      }
+  }
+});
